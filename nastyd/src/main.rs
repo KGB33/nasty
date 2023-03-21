475a246fac91c0be00a041768da21fb0ebdca69b
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-03-21 12:38:40 -0700

    feat(nastyd): Added cli parsing.

--- nastyd/src/main.rs
+++ nastyd/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, value_enum)]
+    mode: Modes
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Modes {
+    Notification,
+    Workspaces,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.mode {
+        Modes::Notification => todo!(),
+        Modes::Workspaces => todo!(),
+    }
 }
