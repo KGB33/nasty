156607c7a6957a12e2b98f5d301afe81415cd787
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-07-07 15:51:24 -0700

    Add Workspace change listener.

--- src/workspaces/sway.rs
+++ src/workspaces/sway.rs
@@ -1,4 +1,19 @@
+use swayipc::{Connection, EventType, Fallible};
+
+fn _listen_and_print() -> Fallible<()> {
+    let subs = [EventType::Workspace];
+    for event in Connection::new()?.subscribe(subs)? {
+        let ws = match event? {
+            swayipc::Event::Workspace(ws) => ws,
+            _ => continue, // We're only listening for Workspace events
+        };
+        println!("{:?}", *ws)
+    }
+    Ok(())
+}
 pub fn listen_and_print() {
-    println!("Call'n sway workspaces!");
-    todo!();
+    match _listen_and_print() {
+        Ok(_) => return,
+        Err(e) => panic!("{}", e),
+    }
 }
